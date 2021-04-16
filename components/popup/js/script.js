6b41f47398741441c5df49acfaf1c10379dffd2b
maskeynihal <maskeynihal@gmail.com>
2021-04-16 10:13:00 +0545

    Add form to add new users for auth

--- components/popup/js/script.js
+++ components/popup/js/script.js
@@ -1,12 +1,87 @@
-const lizzie = document.querySelector("#auth517");
-const cal = document.querySelector("#auth512");
 const count = document.querySelector("#count");
 let requests = 0;
 
-const [lizzie_auth, lizzie_loading] = lizzie.querySelectorAll("button");
-const [cal_auth, cal_loading] = cal.querySelectorAll("button");
+const log = chrome.extension.getBackgroundPage().console.log;
 
-const urls = ["http://localhost", "http://127.0.0.1"];
+const addUser = document.getElementById("addUser");
+const tokenFormContainer = document.getElementById("tokenFormContainer");
+const tokenForm = document.getElementById("tokenForm");
+const removeForm = document.getElementById("removeForm");
+const usersList = document.getElementById("userList");
+const deleteAllToken = document.getElementById("deleteAllToken");
+
+const urls = [
+  "http://localhost",
+  "http://127.0.0.1",
+  "https://dev.vyaguta.lftechnology.com",
+];
+
+deleteAllToken.addEventListener("click", function () {
+  chrome.storage.local.clear(function () {
+    var error = chrome.runtime.lastError;
+    if (error) {
+      log(error);
+      return;
+    }
+
+    updateList();
+  });
+});
+
+function createListWithInnerHTML(users, parentContainer) {
+  const rows = users.map(({ username }) => {
+    const id = username.replaceAll(/\s/g, "");
+    return `
+    <div class="item">
+      <div class="right floated content" id="${id}">
+        <button class="ui button small">Auth</button>
+        <button class="ui loading button small d-none">Loading</button>
+      </div>
+      <img class="ui avatar image" src="https://ui-avatars.com/api/?name=${username}&background=random&size=256" />
+      <div class="content">${username}</div>
+    </div>`;
+  });
+
+  parentContainer.innerHTML = rows.join();
+
+  users.forEach(setupEventListener);
+}
+
+function updateList() {
+  chrome.storage.local.get("vyagutaDevAuthToken", function (result) {
+    const users = result?.vyagutaDevAuthToken ?? [];
+
+    createListWithInnerHTML(users, usersList);
+  });
+}
+
+tokenForm.addEventListener("submit", (e) => {
+  const formData = new FormData(tokenForm);
+  const username = formData.get("username").trim();
+  const token = formData.get("token").trim();
+
+  if (!username || !token) {
+    local.log("Please enter both username and token");
+    return;
+  }
+
+  chrome.storage.local.get("vyagutaDevAuthToken", function (result) {
+    const currentValue = result?.vyagutaDevAuthToken ?? [];
+    const setValue = [...currentValue, { username, token }];
+
+    chrome.storage.local.set({ vyagutaDevAuthToken: setValue }, function () {
+      log(`New user ${username} added.`);
+    });
+  });
+});
+
+removeForm.addEventListener("click", () => {
+  tokenFormContainer.classList.add("d-none");
+});
+
+addUser.addEventListener("click", () => {
+  tokenFormContainer.classList.remove("d-none");
+});
 
 const setToken = ({ accessToken, refreshToken }) => {
   urls.forEach((url) => {
@@ -35,20 +110,27 @@ const setToken = ({ accessToken, refreshToken }) => {
 
 const addEventListener = (button, loading, token) => {
   button.addEventListener("click", () => {
+    log("Clicked");
     button.classList = "ui button small d-none";
     loading.classList = "ui loading button small";
 
-    fetch(`<AUTH_URL>&token=${token}`, {
-      method: "GET",
-    })
+    fetch(
+      `https://dev.vyaguta.lftechnology.com/api/auth/authorize?clientId=lms&token=${token}`,
+      {
+        method: "GET",
+      }
+    )
       .then((res) => res.json())
       .then(({ data }) => {
-        fetch("<FIREBASE>", {
-          method: "POST",
-          body: JSON.stringify({
-            at: new Date().toString(),
-          }),
-        });
+        fetch(
+          "https://vyaguta-extension-default-rtdb.firebaseio.com/requests.json",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              at: new Date().toString(),
+            }),
+          }
+        );
 
         setToken(data);
       })
@@ -59,12 +141,20 @@ const addEventListener = (button, loading, token) => {
   });
 };
 
-addEventListener(lizzie_auth, lizzie_loading, "<ACCESS_TOKEN>");
-addEventListener(cal_auth, cal_loading, "<ACCESS_TOKEN>");
+function setupEventListener({ username, token }) {
+  const id = username.replaceAll(/\s/g, "");
+  const user = document.querySelector("#" + id);
 
-fetch("<FIREBASE>")
+  const [user_auth, user_loading] = user.querySelectorAll("button");
+
+  addEventListener(user_auth, user_loading, token);
+}
+
+fetch("https://vyaguta-extension-default-rtdb.firebaseio.com/requests.json")
   .then((res) => res.json())
   .then((data) => {
     requests = Object.keys(data).length;
     count.innerHTML = requests;
   });
+
+updateList();
