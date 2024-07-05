8dc791833a3b49e7941365276873140d0516c234
Pratik Rajbhandari <c-pratik@memorahealth.com>
2024-07-05 13:38:45 +0545

    Add context menu

--- public/js/background.js
+++ public/js/background.js
@@ -1,1 +1,19 @@
+import browser from "../../src/services/browserExtension";
+import { handleAuthProfile } from "../../src/services/user";
+
 console.log("loaded");
+
+browser?.contextMenus.remove("authProfile");
+
+browser?.contextMenus.create({
+  id: "authProfile",
+  title: "Auth as: %s",
+  contexts: ["link"],
+  targetUrlPatterns: ["https://*.vyaguta.lftechnology.com.np/leapfroggers/*"],
+});
+
+browser?.contextMenus.onClicked.addListener((event) => {
+  if (event.menuItemId === "authProfile") {
+    handleAuthProfile(event.linkUrl);
+  }
+});
